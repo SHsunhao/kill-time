7329de6390cf0e2ca265fdf2248ffd0d47b3e84e
sunhao <872230699@qq.com>
2021-03-02 11:42:48 +0800

    第一次提交

---
+++ model_two/src/main/java/com/zz/model_two/ModelTwoApplication.java
@@ -0,0 +1,13 @@
+package com.zz.model_two;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelTwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelTwoApplication.class, args);
+    }
+
+}
