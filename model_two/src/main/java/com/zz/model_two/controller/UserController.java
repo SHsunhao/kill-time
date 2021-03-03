5905af8ba02b40c92e5f58d5c4320c29f41c85d4
sunhao <872230699@qq.com>
2021-03-03 15:29:11 +0800

    依赖添加版本

--- model_two/src/main/java/com/zz/model_two/controller/UserController.java
+++ model_two/src/main/java/com/zz/model_two/controller/UserController.java
@@ -1,7 +1,8 @@
 package com.zz.model_two.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
--- model_two/src/main/java/com/zz/model_two/web/UserLoginController.java
+++ model_two/src/main/java/com/zz/model_two/web/UserLoginController.java
@@ -2,7 +2,6 @@ package com.zz.model_two.web;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
