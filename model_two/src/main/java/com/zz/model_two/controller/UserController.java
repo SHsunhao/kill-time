33a63797ce09505d26da334b4bb459c8e56c7c89
sunhao <872230699@qq.com>
2021-03-03 14:20:49 +0800

    swagger注解

--- model_two/src/main/java/com/zz/model_two/controller/UserController.java
+++ model_two/src/main/java/com/zz/model_two/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.zz.model_two.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @createTime 2021-03-02 17:08
  */
 @RestController
+@Api(tags = {"controller接口"})
 public class UserController {
 
+    @ApiOperation("con方法")
     @RequestMapping("/hello")
     public String hello(){
         return "hello";
